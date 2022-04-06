cf00ba3ef4a4f3ffdd1c39b5486466bb5ec12666
Sam Sharff <samanthaleec@gmail.com>
2022-04-06 13:12:48 -0400

    adds hardcoded products and v-for loop to display each product on page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,8 +2,12 @@
 export default {
   data: function () {
     return {
-      message: "Wicked awesome cookbook!",
-      messageTwo: "Second message",
+      message: "Wicked awesome store of things!",
+      products: [
+        { id: 1, name: "Shakespeare plays", price: 10 },
+        { id: 2, name: "Jane Austen collection", price: 15 },
+        { id: 3, name: "Charles Dickens: a collection", price: 20 },
+      ],
     };
   },
   created: function () {},
@@ -14,8 +18,14 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <!-- BUILD
-    <BUTTON></BUTTON> -->
+
+    <div>
+      <h1>List of Products</h1>
+    </div>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h2>Name: {{ product.name }}</h2>
+      <h4>Price: {{ product.price }}</h4>
+    </div>
   </div>
 </template>
 
