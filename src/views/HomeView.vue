c848c63ee9c553f9f108996099664af5bce66143
Sam Sharff <samanthaleec@gmail.com>
2022-04-06 17:34:28 -0400

    adds createProduct method including ability to take in params; adds newProductParams to data; adds <input> elements with v-model to take in user input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@ export default {
     return {
       message: "Wicked awesome store of things!",
       products: [],
+      newProductParams: {},
     };
   },
   created: function () {
@@ -18,6 +19,14 @@ export default {
         console.log("All products", this.products);
       });
     },
+    createProduct: function () {
+      console.log("Creating your product");
+
+      axios.post("http://localhost:3000/products.json", this.newProductParams).then((response) => {
+        console.log("Success", response.data);
+        this.products.push(response.data);
+      });
+    },
   },
 };
 </script>
@@ -25,15 +34,32 @@ export default {
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-
     <div>
       <h1>List of Products</h1>
+      <h2>Create that product!</h2>
+      <div>
+        name:
+        <input type="text" v-model="newProductParams.name" />
+        description:
+        <input type="text" v-model="newProductParams.description" />
+        price:
+        <input type="text" v-model="newProductParams.price" />
+        image_url:
+        <input type="text" v-model="newProductParams.image_url" />
+        <!-- </div> -->
+      </div>
     </div>
+    <button v-on:click="createProduct()">Create product</button>
     <div v-for="product in products" v-bind:key="product.id">
       <h2>Name: {{ product.name }}</h2>
+      <img src="image.example.url" :alt="product.name" />
       <h4>Price: {{ product.price }}</h4>
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+img {
+  width: 250px;
+}
+</style>
