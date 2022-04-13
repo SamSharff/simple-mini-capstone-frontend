0d4c65f45d626ca4cb8b8cb32e584103bcedcc14
Sam Sharff <samanthaleec@gmail.com>
2022-04-13 13:27:52 -0400

    adds bootstrap

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -26,6 +26,6 @@ nav a {
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #42b983; */
 }
 </style>
